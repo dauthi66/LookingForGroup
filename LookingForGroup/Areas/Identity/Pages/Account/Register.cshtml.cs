6b0fc474904f1dcfecf508ead859006dd03ecb1e
Kris <dauthi66@gmail.com>
2022-04-13 17:07:02 -0700

    Updated OnPost to save UserName as Username instead of using email (default).

--- LookingForGroup/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LookingForGroup/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -118,7 +118,7 @@ namespace LookingForGroup.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
