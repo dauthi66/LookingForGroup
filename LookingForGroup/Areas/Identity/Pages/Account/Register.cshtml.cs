1ded30e7cbd7f379112dd1298929d02833614c83
Kris <dauthi66@gmail.com>
2022-04-13 17:01:01 -0700

    Added UserName to Register class, and to HTML.

--- LookingForGroup/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LookingForGroup/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,6 +79,10 @@ namespace LookingForGroup.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            public string UserName { get; set; }
+
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
