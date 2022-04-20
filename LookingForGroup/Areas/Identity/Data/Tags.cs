084746f60f2cc8b72dabb9f81c5d443b32c02ceb
Kris <dauthi66@gmail.com>
2022-04-20 16:25:31 -0700

    Added personal data, including a tags class. Testing many to many relationship.

---
+++ LookingForGroup/Areas/Identity/Data/Tags.cs
@@ -0,0 +1,9 @@
+﻿namespace LookingForGroup.Areas.Identity.Data
+{
+    public class Tags
+    {
+        public int TagId { get; set; }
+
+        public string? TagName { get; set; }
+    }
+}
--- LookingForGroup/Data/Migrations/20220420232350_test1.Designer.cs
+++ LookingForGroup/Data/Migrations/20220420232350_test1.Designer.cs
@@ -4,6 +4,7 @@ using LookingForGroup.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LookingForGroup.Data.Migrations
 {
     [DbContext(typeof(LookingForGroupDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220420232350_test1")]
+    partial class test1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
