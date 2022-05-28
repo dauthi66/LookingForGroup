c585259de9f24c159fd72038d36e23a619cf2fe0
MejiaJ84 <jtmejia1984@gmail.com>
2022-05-28 15:43:51 -0700

    Added user manager to ManageTags model

--- LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
+++ LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
@@ -1,3 +1,5 @@
+using LookingForGroup.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,5 +7,15 @@ namespace LookingForGroup.Areas.Identity.Pages.Account.Manage
 {
     public class ManageTagsModel : PageModel
     {
+        private readonly UserManager<LookingForGroupUser> _userManager;
+        private readonly SignInManager<LookingForGroupUser> _signInManager;
+
+        public ManageTagsModel(
+            UserManager<LookingForGroupUser> userManager,
+            SignInManager<LookingForGroupUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
     }
 }
