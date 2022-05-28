5ac6385df335356272c09c9e9f89a9c9d6294d7a
MejiaJ84 <jtmejia1984@gmail.com>
2022-05-28 16:16:42 -0700

    Started to build out the ManageTagsModel InputModel

--- LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
+++ LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using LookingForGroup.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,21 @@ namespace LookingForGroup.Areas.Identity.Pages.Account.Manage
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public class InputModel
+        {
+            
         }
     }
 }
