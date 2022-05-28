33856920675e0cf056e80a25a376bd26d1621184
MejiaJ84 <jtmejia1984@gmail.com>
2022-05-28 15:03:37 -0700

    Adding Manage Tags page for users

---
+++ LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LookingForGroup.Areas.Identity.Pages.Account.Manage
+{
+    public class ManageTagsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
