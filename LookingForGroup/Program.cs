6848fc78af9f31f852b8a1b39bf221dfe43c7d87
MejiaJ84 <jtmejia1984@gmail.com>
2022-06-03 15:36:55 -0700

    Published website to azure and fixed the default connection string in appsettings.json and secrets folder to be able to run test locally

--- LookingForGroup/Program.cs
+++ LookingForGroup/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<LookingForGroupDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddTransient<TagsDBHelper>();
