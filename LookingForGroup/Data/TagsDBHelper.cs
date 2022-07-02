7dc21dcb5640bb38ae98c8fec53b1749cd6a961d
Kris <dauthi66@gmail.com>
2022-07-02 12:54:35 -0700

    Documented TagsDBHelper class and methods. Cleaned up code a bit.

--- LookingForGroup/Data/TagsDBHelper.cs
+++ LookingForGroup/Data/TagsDBHelper.cs
@@ -3,20 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LookingForGroup.Data
 {
+    /// <summary>
+    /// Database access commands to assist in utlizing Tags
+    /// </summary>
     public class TagsDBHelper
     { 
         private readonly LookingForGroupDbContext _context;
         
-
         public TagsDBHelper(LookingForGroupDbContext context)
         {
             _context = context;
-            
         }
 
+        /// <summary>
+        /// Creates a new tag with given name if
+        /// that tag does not currently exist in database
+        /// </summary>
+        /// <param name="newTag">The name of the tag you want to create</param>
         public void addTag(string newTag)
-        {
-            
+        {       
             Tags? tag = (from Tags in _context.Tags
                          where Tags.TagName == newTag
                          select Tags).FirstOrDefault();
@@ -32,6 +37,11 @@ namespace LookingForGroup.Data
             }
         }
 
+        
+        /// <summary>
+        /// Gets a list of all tags currently in database
+        /// </summary>
+        /// <returns>A list of all tags in the database</returns>
         public async Task<List<Tags>> getTagsList()
         {
             return await _context.Tags.ToListAsync();
