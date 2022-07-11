466e06a2bd55cee19b04afa3268a76ecb2150b3b
Kris <dauthi66@gmail.com>
2022-07-10 18:10:27 -0700

    Added some helpful information for ManageTags model and page.

--- LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
+++ LookingForGroup/Areas/Identity/Pages/Account/Manage/ManageTags.cshtml.cs
@@ -23,16 +23,11 @@ namespace LookingForGroup.Areas.Identity.Pages.Account.Manage
 
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public InputModel Input { get; set; } = null!;
 
         [TempData]
         public string StatusMessage { get; set; } = null!;
-
 
         //input model for tag list 
         public class InputModel
